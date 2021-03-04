9985b5eddfaa5764f904ad004666d7d9737637d3
bencbabcock01 <bencbabcock01@gmail.com>
2021-03-04 16:44:08 +0000

    remove old stuff

--- forthefuture/settings.py
+++ forthefuture/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'f1^qn+f5quv=%ylimqy0bu*aza#5-=m=0)y=2g6_n+9s+dw=9!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bencbabcock01.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
